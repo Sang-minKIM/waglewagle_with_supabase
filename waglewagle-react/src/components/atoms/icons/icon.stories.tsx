58d3c49c6be99b7ce828503e8dd58ecc2c2dc85c
Sang-minKIM <87116017+Sang-minKIM@users.noreply.github.com>
2024-05-17 18:11:37 +0900

    feat: add icon stories

---
+++ waglewagle-react/src/components/atoms/icons/icon.stories.tsx
@@ -0,0 +1,40 @@
+import { Icons } from "./index";
+
+export default {
+  title: "Icons",
+  component: Icons,
+};
+
+export const PinIcons = () => (
+  <>
+    <Icons.Pin0 />
+    <Icons.Pin1 />
+    <Icons.Pin2 />
+    <Icons.Pin3 />
+    <Icons.Pin4 />
+    <Icons.Pin5 />
+    <Icons.Pin6 />
+    <Icons.Pin7 />
+    <Icons.Pin8 />
+    <Icons.Pin9 />
+    <Icons.Pin10 />
+    <Icons.Pin11 />
+  </>
+);
+
+export const TagIcons = () => (
+  <>
+    <Icons.Tag0 />
+    <Icons.Tag1 />
+    <Icons.Tag2 />
+    <Icons.Tag3 />
+    <Icons.Tag4 />
+    <Icons.Tag5 />
+    <Icons.Tag6 />
+    <Icons.Tag7 />
+    <Icons.Tag8 />
+    <Icons.Tag9 />
+    <Icons.Tag10 />
+    <Icons.Tag11 />
+  </>
+);
