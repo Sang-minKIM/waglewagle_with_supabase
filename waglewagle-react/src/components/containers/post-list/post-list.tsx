2220563736df9d361ce343f1994b9fc9cb9a5272
Sang-minKIM <87116017+Sang-minKIM@users.noreply.github.com>
2024-06-18 13:35:49 +0900

    feat: add empty list view

--- waglewagle-react/src/components/containers/post-list/post-list.tsx
+++ waglewagle-react/src/components/containers/post-list/post-list.tsx
@@ -1,9 +1,9 @@
-import { PostList } from "~/organisms/post";
+import { EmptyList, PostList } from "~/organisms/post";
 
 import { usePostListQuery } from "./post-list.api";
 
 export const PostListContainer = ({ stationId }: { stationId: string }) => {
   const { data } = usePostListQuery(stationId);
 
-  return <PostList posts={data.posts} />;
+  return data.posts.length ? <PostList posts={data.posts} /> : <EmptyList message="나의 지하철 소식을 공유해보세요!" />;
 };
